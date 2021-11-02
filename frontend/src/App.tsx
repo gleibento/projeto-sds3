36a0dae353b47de71f934fc3d275da893da4c1f0
gleibento <glei-bento@hotmail.com>
2021-11-02 13:06:41 -0300

    add navbar and footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,16 @@
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
 
 function App() {
   return (
-    <div>
-     <h1 className="text-primary">Home</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary">Home</h1>
+
+      </div>
+      <Footer/>
+    </>
   );
 }
 
